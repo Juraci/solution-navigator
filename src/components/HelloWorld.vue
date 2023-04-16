af149fc3be98a96b6b752f5d75eac3496fe21320
Juraci <juraci.vieira@gmail.com>
2023-04-16 18:51:08 -0300

    linting

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,9 @@
 defineProps({
   msg: {
     type: String,
-    required: true
-  }
-})
+    required: true,
+  },
+});
 </script>
 
 <template>
@@ -22,7 +22,7 @@ defineProps({
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
-  top: -10px
+  top: -10px;
 }
 
 h3 {

48e4e6cfdfc7396e422284e7a56ca4dfa1734b3f
Juraci <juraci.vieira@gmail.com>
2023-04-18 10:53:46 -0300

    more primeVue configuration

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import PrimeVue from 'primevue/config';
 import App from './App.vue';
 import router from './router';
 
-const app = createApp(App);
+import './assets/app.css';
 
 // PrimeVue
 import 'primevue/resources/themes/md-dark-indigo/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
 
+const app = createApp(App);
 app.use(router);
 app.use(PrimeVue);
 

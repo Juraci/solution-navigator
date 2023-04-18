ce2cd7751d5a3092d4174cb6090acd168e7dcb08
Juraci <juraci.vieira@gmail.com>
2023-04-18 11:28:56 -0300

    adding pinia for state management

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import PrimeVue from 'primevue/config';
 import App from './App.vue';
 import router from './router';
@@ -13,5 +14,6 @@ import 'primeicons/primeicons.css';
 const app = createApp(App);
 app.use(router);
 app.use(PrimeVue);
+app.use(createPinia());
 
 app.mount('#app');

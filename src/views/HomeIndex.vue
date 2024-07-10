a841a5f1d874ccdbac02f6a28bbea4102cda236f
Corey Condardo <ccondardo@gmail.com>
2024-07-10 10:45:59 -0400

    refactor

--- src/views/HomeIndex.vue
+++ src/views/HomeIndex.vue
@@ -5,11 +5,14 @@ import ConfirmDialog from 'primevue/confirmdialog';
 import { useRouter, useRoute } from 'vue-router';
 import { useNodeStore } from '@/stores/NodeStore';
 import { useConfirm } from 'primevue/useconfirm';
+import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
 
 const router = useRouter();
 const route = useRoute();
-const { addNode, deleteNode, saveStore } = useNodeStore();
 const confirm = useConfirm();
+const { nodes } = storeToRefs(useNodeStore());
+const { addNode, deleteNode } = useNodeStore;
 
 const handleAddNode = () => {
   const uuid = addNode();
@@ -36,22 +39,47 @@ const handleNodeNotFound = () => {
   router.push({ name: 'node.not-found' });
 };
 
-const handleSaveStore = () => {
-  saveStore();
-};
+// const handleSaveStore = () => {
+//   const nodesJson = JSON.parse(JSON.stringify(nodes.value));
+//   const nodesString = JSON.stringify(nodesJson);
+
+//   // create the downloadable file
+//   const blob = new Blob([nodesString], { type: 'application/json' });
+//   const url = URL.createObjectURL(blob);
+//   const link = document.createElement('a');
+//   link.href = url;
+//   link.download = 'solutionNavigatorSave.json'; // Name of the file to be downloaded
+//   document.body.appendChild(link); // Append the link to the body
+//   link.click(); // Programmatically click the link to trigger the download
+//   document.body.removeChild(link); // Remove the link from the body
+//   URL.revokeObjectURL(url); // Clean up the URL object
+// };
+
+const downloadNodesLink = computed(() => {
+  const jsonNodes = JSON.stringify(nodes.value);
+  const blob = new Blob([jsonNodes], { type: 'application/json' });
+
+  return URL.createObjectURL(blob);
+});
 </script>
 
 <template>
   <div class="container">
     <div class="side-panel">
       <div class="side-panel-header">
-        <Button icon="pi pi-save" aria-label="Save" severity="secondary" @click="handleSaveStore" />
+        <Button
+          icon="pi pi-save"
+          aria-label="Save"
+          severity="secondary"
+          as="a"
+          :href="downloadNodesLink"
+          :download="`solution-navigator-${new Date().toISOString()}.json`"
+        />
         <Button
           data-test-create-node
           icon="pi pi-pen-to-square"
           aria-label="Create Node"
           severity="secondary"
-          @click="handleAddNode"
         />
       </div>
       <NodesArbor />
